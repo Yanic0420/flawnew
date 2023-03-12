5001dea13161bbdfc35b30d7782107edb10bb8e9
Yanic0420 <zhouyanic0420@gmail.com>
2023-03-12 17:23:21 +0800

    Initial commit

---
+++ DataProcessing.py
@@ -0,0 +1,12 @@
+import torch
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+from d2l import torch as d2l
+
+d2l.use_svg_display()
+
+#通过ToTensor实例将图像数据从PIL类型变换成32位浮点数格式
+#并除以255使得所有像素的数值均在0到1之间
+trans=transforms.ToTensor()
+
